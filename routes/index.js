1383849c595e4d6b042eff0b72d85b362ff0228b
Nicktam32 <nicktam22@gmail.com>
2024-04-26 17:46:31 +0800

    add simple notes

--- routes/index.js
+++ routes/index.js
@@ -6,8 +6,13 @@ router.get('/', function(req, res, next) {
   res.render('index', { title: 'Express' });
 });
 
-/* GET home page. */
+/* GET old_version page. */
 router.get('old_version', function(req, res, next) {
   res.render('old_version', { title: 'Express' });
+});
+
+/* GET notes page. */
+router.get('notes', function(req, res, next) {
+  res.render('notes', { title: 'Express' });
 });
 module.exports = router;
